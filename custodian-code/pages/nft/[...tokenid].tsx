ee288d6bedfb79493443d02ab7a792e6ec3e42b5
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2023-02-16 01:31:31 +0530

    fix: auth
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- custodian-code/pages/nft/[...tokenid].tsx
+++ custodian-code/pages/nft/[...tokenid].tsx
@@ -2,10 +2,13 @@ import { getNftByTokenId } from '@/utils/nft';
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react';
 import { INFTItemEx } from '@/types/index'
+import { useSession } from 'next-auth/react';
 
 export default function Nft() {
 
   const router = useRouter()
+
+  const { data, status } = useSession()
 
 
   const [nft, setNft] = useState<INFTItemEx>()
@@ -35,8 +38,7 @@ export default function Nft() {
           <div className="lg:w-4/5 mx-auto flex flex-wrap">
             <img src={nft?.image} alt={nft?.name} className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" />
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0"   >
-              <h2 className="text-sm title-font text-gray-500 tracking-widest"   >{nft?.name}</h2>
-
+              <h2 className="text-xl title-font text-gray-300 tracking-widest"   >{nft?.name}</h2>
               <div className="flex mb-4">
                 <span className="flex items-center">
                   <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
@@ -54,7 +56,7 @@ export default function Nft() {
                   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
                     <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                   </svg>
-                  <span className="text-gray-600 ml-3">20 Reviews</span>
+                  <span className="text-gray-600 ml-3">{nft?.tokenId} Reviews</span>
                 </span>
                 <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
                   <a className="text-gray-500">
@@ -74,15 +76,20 @@ export default function Nft() {
                   </a>
                 </span>
               </div>
-              <p className="leading-relaxed pb-5">{nft?.description}</p>
+              <p className="leading-relaxed pb-5 text-justify">{nft?.description}</p>
               <div className="flex">
-                <span className=" title-font font-medium text-2xl text-white">{nft?.price}</span>
-                <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Buy</button>
-                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                <span className=" title-font font-medium text-2xl text-white">{nft?.price} {nft?.isFiat === true ? "USD" : "BIT"}</span>
+
+
+
+                <button className="rounded-full  w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                   <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
                     <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
                   </svg>
                 </button>
+
+                {status === "authenticated" && <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Buy</button>}
+
               </div>
             </div>
           </div>
