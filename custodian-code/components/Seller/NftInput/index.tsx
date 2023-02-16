cbd1f02ff585523d9c84cc38ba92400456cdce4f
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2023-02-17 02:17:43 +0530

    add: fix warnings
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- custodian-code/components/Seller/NftInput/index.tsx
+++ custodian-code/components/Seller/NftInput/index.tsx
@@ -51,6 +51,8 @@ const NftInput: React.FC<IProps> = ({ setNftName, setNftDescription, setPrice, n
         }
     }
 
+    function handleDummy() { }
+
 
     return (
         <>
@@ -77,7 +79,7 @@ const NftInput: React.FC<IProps> = ({ setNftName, setNftDescription, setPrice, n
                 <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                     <div className="space-y-1 text-center">
                         <svg className="mx-auto h-12 w-12 text-white" stroke="currentColor" fill="none" viewBox="0 0 48 48">
-                            <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
                         <div className="flex text-sm text-gray-600">
                             <label className="relative cursor-pointer  rounded-md font-medium text-slate-200 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
@@ -100,22 +102,22 @@ const NftInput: React.FC<IProps> = ({ setNftName, setNftDescription, setPrice, n
                         <p>Payment Should be in </p><br />
 
                         <li onClick={e => store.dispatch(setPayWith(IPaymentType.CRYPTO))}>
-                            <input type="radio" name="paymentType" value={IPaymentType.CRYPTO} checked={payWith === IPaymentType.CRYPTO} className="hidden peer" />
+                            <input type="radio" name="paymentType" value={IPaymentType.CRYPTO} onChange={handleDummy} checked={payWith === IPaymentType.CRYPTO} className="hidden peer" />
                             <label className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
                                 <div className="block">
                                     <div className="w-full text-lg font-semibold">Crypto</div>
                                 </div>
-                                <svg aria-hidden="true" className="w-6 h-6 ml-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                <svg aria-hidden="true" className="w-6 h-6 ml-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                             </label>
                         </li>
                         <li onClick={e => store.dispatch(setPayWith(IPaymentType.CREDIT_CARD))}>
-                            <input type="radio" name="paymentType" value={IPaymentType.CREDIT_CARD}
+                            <input type="radio" name="paymentType" onChange={handleDummy} value={IPaymentType.CREDIT_CARD}
                                 className="hidden peer" checked={payWith === IPaymentType.CREDIT_CARD} />
                             <label className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
                                 <div className="block">
                                     <div className="w-full text-lg font-semibold">Credit Card</div>
                                 </div>
-                                <svg aria-hidden="true" className="w-6 h-6 ml-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                <svg aria-hidden="true" className="w-6 h-6 ml-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                             </label>
                         </li>
                     </ul>
--- custodian-code/components/Seller/WalletStatus/index.tsx
+++ custodian-code/components/Seller/WalletStatus/index.tsx
@@ -20,28 +20,30 @@ const CheckWalletStatus: React.FC<IProps> = ({ }) => {
 
     useSelector((state: IStore) => state.walletAddress)
 
+    function handleDummy() { }
+
 
     return (
         <>
             <div className="px-24">
                 <ul className="grid w-full gap-6 md:grid-cols-2 pt-5">
                     <li onClick={(e) => store.dispatch(setUserHasWallet(false))}>
-                        <input type="radio" name="walletType" checked={userHasWallet === false} className="hidden peer" />
+                        <input type="radio" name="walletType" checked={userHasWallet === false} className="hidden peer" onChange={handleDummy} />
                         <label className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
                             <div className="block">
                                 <div className="w-full text-lg font-semibold">No Wallet</div>
                             </div>
-                            <svg aria-hidden="true" className="w-6 h-6 ml-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            <svg aria-hidden="true" className="w-6 h-6 ml-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                         </label>
                     </li>
                     <li onClick={(e) => store.dispatch(setUserHasWallet(true))}>
                         <input type="radio" name="walletType"
-                            className="hidden peer" checked={userHasWallet === true} />
+                            className="hidden peer" checked={userHasWallet === true} onChange={handleDummy} />
                         <label className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
                             <div className="block">
                                 <div className="w-full text-lg font-semibold">Wallet</div>
                             </div>
-                            <svg aria-hidden="true" className="w-6 h-6 ml-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            <svg aria-hidden="true" className="w-6 h-6 ml-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                         </label>
                     </li>
                 </ul>
