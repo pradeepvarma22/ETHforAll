acc97a6259257e0b675b5f8a816f10004e17a7d2
Pradeepvarma_22 <pradeepnadimpalli22@gmail.com>
2023-02-19 18:51:59 +0530

    Update rpc_network.ts

--- custodian-code/rpc_network.ts
+++ custodian-code/rpc_network.ts
@@ -1,6 +1,7 @@
 export const MUMBAI: string = "https://matic-mumbai.chainstacklabs.com"
 export const HYPERSPACE: string = "https://api.hyperspace.node.glif.io/rpc/v1"
 export const MANTLE: string = "https://rpc.testnet.mantle.xyz"
-export const MANTLE_QUICK_NODE: string = process.env.NEXT_PUBLIC_MANTLE_URI_QUICKNODE!
-export const HYPERSPACE_QUICK_NODE: string = process.env.NEXT_PUBLIC_HYPERSPACE_URI_QUICKNODE!
-export const MUMBAI_QUICK_NODE: string = process.env.NEXT_PUBLIC_MUMBAI_URI_ALCHEMY!
+export const MANTL_: string = process.env.NEXT_PUBLIC_MANTLE_URI_!
+export const HYPERSPACE: string = process.env.NEXT_PUBLIC_HYPERSPACE_URI_!
+export const MUMBAI_ALCHEMY: string = process.env.NEXT_PUBLIC_MUMBAI_URI_ALCHEMY!
+export const MUMBAI_QUICK_NODE: string = process.env.NEXT_PUBLIC_MUMBAI_URI_QUICKNODE!
